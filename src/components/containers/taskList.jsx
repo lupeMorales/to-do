f3f29bb42cb036a0bf6f8a0406dfd02b50d750ed
lupeMorales <morales.lupegmail.com>
2022-10-20 10:59:30 +0200

    solve render task list

--- src/components/containers/taskList.jsx
+++ src/components/containers/taskList.jsx
@@ -1,19 +1,37 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LEVELS } from "../../models/levels.enum";
 import { TaskClass } from "../../models/taskClass.class";
 import Task from "../pures/task";
 
 const TaskList = () => {
-  const task1 = new TaskClass("task", "description", false, LEVELS.URGENT);
-  const task2 = new TaskClass("task2", "description", false, LEVELS.NORMAL);
+  const task1 = new TaskClass("taskis", "description", true, LEVELS.URGENT);
+  const task2 = new TaskClass("taskta", "description", false, LEVELS.NORMAL);
   const task3 = new TaskClass("task3", "description", false, LEVELS.BLOCKING);
 
-  const [taskList, setTaskList] = useState([task1, task2, task3]);
+  const [tasks, setTasks] = useState([task1, task2, task3]);
+
+  const [loading, setLoading] = useState(true);
+
+  //control del ciclo de vida del componente
+
+  useEffect(() => {
+    console.log("update takList state");
+    setLoading(false);
+  }, [tasks]);
+
+  //change task status: completed/not completed
+  function completeTask(task) {
+    console.log("complete mi siela", task);
+    const index = tasks.indexOf(task);
+    const tempList = [...tasks];
+    tempList[index].completed = !tempList[index].completed;
+    setTasks(tempList);
+  }
   return (
     <section>
       <ul>
-        {taskList.map((item, index) => {
-          return <Task key={index} task={item} />;
+        {tasks.map((item, index) => {
+          return <Task key={index} task={item} complete={completeTask} />;
         })}
       </ul>
     </section>
--- src/components/pures/task.jsx
+++ src/components/pures/task.jsx
@@ -1,19 +1,74 @@
-import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 import "../../styles/components/task.scss";
 import { TaskClass } from "../../models/taskClass.class";
+import { LEVELS } from "../../models/levels.enum";
 
-const Task = ({ task }) => {
+const Task = ({ task, complete }) => {
+  useEffect(() => {
+    console.log("created task");
+    return () => {
+      console.log(`task: ${task.name} is going to unmount`);
+    };
+  }, [task]);
+
+  //function that return a budge dependin on the level of the task
+  function levelBadge() {
+    switch (task.level) {
+      case LEVELS.NORMAL:
+        return (
+          <div className=" task__level task__level-normal">
+            <p className="task__level-text">{task.level}</p>
+          </div>
+        );
+      case LEVELS.BLOCKING:
+        return (
+          <div className="task__level task__level-blocking">
+            <p className="task__level-text">{task.level}</p>
+          </div>
+        );
+      case LEVELS.URGENT:
+        return (
+          <div className="task__level task__level-urgent">
+            {" "}
+            <p className="task__level-text">{task.level}</p>
+          </div>
+        );
+      default:
+        break;
+    }
+  }
+
+  //function that return icon depending on the status task: completed/not completed
+  function taskCompletedIcon() {
+    if (task.completed) {
+      return (
+        <i
+          className="fa-sharp fa-solid fa-circle-check fa-lg "
+          style={{ color: "green" }}
+          onClick={() => complete(task)}
+        ></i>
+      );
+    } else {
+      return (
+        <i
+          className="fa-regular fa-circle fa-lg "
+          style={{ color: "grey" }}
+          onClick={() => complete(task)}
+        ></i>
+      );
+    }
+  }
   return (
     <li className="task">
       <article className="task__container">
-        <div className="task__level">{task.level}</div>
+        {levelBadge()}
 
         <h2 className="task__title">{task.name}</h2>
         <h3 className="task__subtitle">{task.description}</h3>
         <div className="task__actions">
-          <i class="fa-regular fa-circle fa-lg"></i>
+          {taskCompletedIcon()}
           <i class="fa-regular fa-trash-can fa-lg"></i>
         </div>
       </article>
@@ -23,6 +78,7 @@ const Task = ({ task }) => {
 
 Task.propTypes = {
   task: PropTypes.instanceOf(TaskClass).isRequired,
+  complete: PropTypes.func.isRequired,
 };
 
 export default Task;
